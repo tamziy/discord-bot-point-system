4b3eff41d7c63d5cbd76ef8eaa3ded48af145a6d
tamziy <114886626+tamziy@users.noreply.github.com>
2024-09-06 20:13:26 -0500

    Add DynamoDB setup and connection

--- app/bot.py
+++ app/bot.py
@@ -1,9 +1,46 @@
 from dotenv import load_dotenv
 import os
 import discord
+import boto3
 
+#load the info from the env file into the environmental variables
 load_dotenv()
 
+#AWS
+dynamodb = boto3.resource (
+    'dynamodb',
+    region_name = os.getenv('AWS_REGION'),
+    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+)
+
+table = dynamodb.Table(os.getenv('DYNAMODB_TABLE_NAME'))
+
+print(f"Connected to DynamoDB table: {table.table_name}")
+
+print("Ensuring connection...")
+
+table.put_item(
+    Item={
+        'user_id': 'test_user',
+        'points' : 100
+    }
+)
+
+response = table.get_item(Key = {'user_id': 'test_user'})
+if 'Item' in response:
+    print(f"Connection ensured. Response: {response['Item']}")
+    
+    print("Deleting test user...")
+    table.delete_item(
+        Key={
+            'user_id': 'test_user'
+        }
+    )
+else:
+    print("Error X-X: Test user not found. Table potentially not connected.")
+
+#Discord
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
 intents = discord.Intents.default()
